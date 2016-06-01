8f24636c4b754a151f7bce603f2648401f76d465
Luiz Carlos <luizcarlos1405@gmail.com>
2016-06-01 16:00:58 -0400

    jump working

---
+++ objects/player.lua
@@ -0,0 +1,58 @@
+local player = collider:rectangle(G_width / 2, G_height - 100, 32, 32)
+
+player.vel      = {x = 0, y = 0}
+player.speed    = 200
+player.maxvel   = 500
+player.gravity  = 200
+player.friction = 0.8
+player.jump     = 1000
+player.jumptime = 0.3
+player.fuel     = 0
+
+function player:update(dt)
+	-- Controls
+	if love.keyboard.isDown("a") then
+		self.vel.x = self.vel.x - self.speed
+	end
+	if love.keyboard.isDown("d") then
+		self.vel.x = self.vel.x + self.speed
+	end
+	if love.keyboard.isDown("space") and self.fuel > 0 then
+		self.vel.y = self.vel.y - self.jump
+		self.fuel  = self.fuel - dt
+		end
+
+	-- Gravity
+	self.vel.y = self.vel.y + self.gravity
+	if math.abs(self.vel.x) > self.maxvel then self.vel.x = self.maxvel * (math.abs(self.vel.x) / self.vel.x) end
+	if math.abs(self.vel.y) > self.maxvel then self.vel.y = self.maxvel * (math.abs(self.vel.y) / self.vel.y) end
+
+	-- Frition
+	self.vel = {x = self.vel.x * self.friction, y = self.vel.y * self.friction}
+
+	-- Position atualization
+	self:move(self.vel.x * dt, self.vel.y * dt)
+
+	-- Colision handler
+	for _, delta in pairs(collider:collisions(self)) do
+		self:move(delta.x, delta.y)
+		if delta.x == 0 then
+			self.vel.fuel = 0
+			self.fuel = self.jumptime
+		end
+	end
+end
+
+function player:draw()
+	local x1, y1, x2, y2 = self:bbox()
+	love.graphics.points(x1, x2)
+	love.graphics.rectangle("fill", x1, y1, 32, 32)
+end
+
+function player:keyreleased(key)
+	if key == "space" then
+		self.fuel = 0
+	end
+end
+
+return player
--- states/game.lua
+++ states/game.lua
@@ -1,46 +1,37 @@
 game = {}
 
 -- Necessary requires
-local HC = require("tools/HC")
 
 -- require("classes/rectangle")
 
 function game:enter()
 	-- Physics
+	HC = require("tools/HC")
 	collider = HC.new()
+
 		-- Objects
+	player = require("objects/player")
 	floor  = collider:rectangle(0, G_height - 20, G_width, 200)
-	rect   = collider:rectangle(110, 90, 200, 400)
-	rect2  = collider:rectangle(400, 800, 200, 200)
 	-- rect = Rectangle.init(rect, {x = 0, y = 0}, 20, 20)
 end
 
 function game:update(dt)
+	player:update(dt)
 
-	if love.keyboard.isDown("a") then
-		rect:move(-10, 0)
-	end
-	if love.keyboard.isDown("d") then
-		rect:move(10, 0)
-	end
-	if love.keyboard.isDown("space") then
-		rect:move(0, -20)
-	end
-	rect:move(0, 10)
-
-	for _, delta in pairs(collider:collisions(rect)) do
-		rect:move(delta.x, delta.y)
-	end
 end
 
 function game:draw()
 	push:apply("start")
 
-	floor:draw()
-	rect:draw()
-	rect2:draw()
+	floor:draw("fill")
+	player:draw("fill")
 
 	push:apply("end")
+	love.graphics.print(player.vel)
+end
+
+function game:keyreleased(key)
+	player:keyreleased(key)
 end
 
 return game
